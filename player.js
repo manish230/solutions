a0acecf3c839806912edcaf0839d5601a3f75c6b
manish230 <jhamanish230@gmail.com>
2017-02-27 15:07:40 +0530

    player.js

---
+++ player.js
@@ -0,0 +1,14 @@
+var audioOption={channels:2,bitDepth:16,sampleRate:44100};
+ lame=require("lame");
+ colors=require('colors')
+speaker=require('speaker')
+fs=require('fs');
+song=process.argv[2];
+var path=require('path')
+ inputstream=fs.createReadStream(process.cwd()+'/'+song);
+ decoder= new lame.Decoder(audioOption);
+decoder.on('format',function(){
+    console.log(colors.red("decoded successfully"));
+});
+speaker=new speaker(audioOption)
+inputstream.pipe(decoder).pipe(speaker);
